4c9163e31e0008b3d9a1e6a74266dd70ddfcf770
Andrew <apollock92@hotmail.com>
2018-11-14 01:18:36 +0000

    Functionalise the scraping.
    Updated to scrape both UK and US Amazon pages.

--- func_scrape_amazon_reviews.R
+++ func_scrape_amazon_reviews.R
@@ -1,45 +1,89 @@
 
 ### Creating a generalisable function to download all the reviews for a given product ID
 
-library(dplyr)
-library(rvest)
-library(purrr)
+extract_reviews <- function(product_id = NULL) {
 
-
-### Function inputs
-product_id <- "0553296124"
-
+  library(dplyr)
+  library(rvest)
+  library(purrr)
+  library(lubridate)
 
 
 # Find out how many pages of reviews there are
-base_url <- paste0("https://www.amazon.co.uk/product-reviews/", product_id)
+  uk_url <- paste0("https://www.amazon.co.uk/product-reviews/", product_id)
+  us_url <- paste0("https://www.amazon.com/product-reviews/", product_id)
 
-num_of_pages <- base_url %>% 
-  read_html() %>% 
-  html_nodes('.totalReviewCount') %>%      
-  html_text() %>% 
-  as.integer() %>% `/`(10) %>% round()
+  num_of_pages_uk <- uk_url %>% 
+    read_html() %>% 
+    html_nodes('.totalReviewCount') %>%      
+    html_text() %>% 
+    as.integer() %>% `/`(10) %>% ceiling()
 
+  num_of_pages_us <- us_url %>%
+    read_html() %>%
+    html_nodes('.totalReviewCount') %>%
+    html_text() %>%
+    as.integer() %>% `/`(10) %>% ceiling()
+  
+  
+  datalist <- list()
 
+  # Iterate through UK reviews
+  for (page_number in 1:num_of_pages_uk) {
+    
+    loop_url <- paste0("https://www.amazon.co.uk/product-reviews/", product_id, "/?pageNumber=", page_number)
 
-# Now iterate through each page of reviews, extracting the 10 reviews
+    Sys.sleep(runif(1, 3, 5)) ## Pause for 3 to 5 seconds
 
-#loop_url <- paste0("https://www.amazon.co.uk/product-reviews/", product_id, "/?pageNumber=", page_num)
-loop_url <- paste0("https://www.amazon.co.uk/product-reviews/", product_id, "/?pageNumber=", "3")
-
-df <- read_html(loop_url) %>% 
-  html_nodes('.view-point .a-col-left') %>% 
-  map_df(~list(Reviewer = html_nodes(.x, '.a-profile-name') %>% 
-                 html_text(),
-               Date = html_nodes(.x, '.review-date') %>% 
-                 html_text(),
-               Title = html_nodes(.x, '#cm_cr-review_list .a-color-base') %>% 
-                 html_text(),
-               Review_Text = html_nodes(.x, '.review-text') %>% 
-                 html_text(),
-               Review_Score = html_nodes(.x, 'span.a-icon-alt') %>% 
-                 html_text() %>% substr(1, 3) %>% as.numeric()
+    df <- read_html(loop_url) %>% 
+      html_nodes('.view-point .a-col-left') %>% 
+      map_df(~list(Reviewer = html_nodes(.x, '.a-profile-name') %>% 
+                     html_text(),
+                   Date = html_nodes(.x, '.review-date') %>% 
+                     html_text(),
+                   Title = html_nodes(.x, '#cm_cr-review_list .a-color-base') %>% 
+                     html_text(),
+                   Review_Text = html_nodes(.x, '.review-text') %>% 
+                     html_text(),
+                   Review_Score = html_nodes(.x, 'span.a-icon-alt') %>% 
+                     html_text() %>% substr(1, 3) %>% as.numeric()
+                  )
               )
-          )
 
+    df$Country <- "UK"
+    df$Date <- dmy(df$Date)
+    datalist[[page_number]] <- df
+    
+  }  
 
+  # Iterate through US reviews
+  for (page_number in 1:num_of_pages_us) {
+    
+    loop_url <- paste0("https://www.amazon.com/product-reviews/", product_id, "/?pageNumber=", page_number)
+    
+    Sys.sleep(runif(1, 3, 5)) ## Pause for 3 to 5 seconds
+    
+    df <- read_html(loop_url) %>% 
+      html_nodes('.view-point .a-col-left') %>% 
+      map_df(~list(Reviewer = html_nodes(.x, '.a-profile-name') %>% 
+                     html_text(),
+                   Date = html_nodes(.x, '.review-date') %>% 
+                     html_text(),
+                   Title = html_nodes(.x, '#cm_cr-review_list .a-color-base') %>% 
+                     html_text(),
+                   Review_Text = html_nodes(.x, '.review-text') %>% 
+                     html_text(),
+                   Review_Score = html_nodes(.x, 'span.a-icon-alt') %>% 
+                     html_text() %>% substr(1, 3) %>% as.numeric()
+                  )
+              )
+    
+    df$Country <- "US"
+    df$Date <- mdy(df$Date)
+    datalist[[page_number+num_of_pages_uk]] <- df
+    
+  }
+  
+  combined_review_data <- bind_rows(datalist)
+
+}
